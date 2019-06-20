042ddc1e678b912f86a77ed47274c289c9b8f39d
abrasumente <abrasumentee@gmail.com>
2019-06-20 11:23:21 +0800

    fuck2

--- py-frontend/src/App.js
+++ py-frontend/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BroswerRouter as Router, Route, Link} from "react-router-dom"
+
 import './App.css';
+import Entry from './entry'
+import GrabTicket from './GrabTicket'
+import MyTickets from './MyTicket'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <Router>
+
+          <header>好想去看！！</header>
+
+          <Link to="/grab">
+            <button className="entry-button">我要抢票！</button>
+          </Link>
+          <Link to="/tickets">
+            <button className="entry-button">我抢到票没？</button>
+          </Link>
+          
+
+          <Route path="/" exact component={Entry} />
+          <Route path="/tickets" component={MyTickets} />
+          <Route path="/grab"  component={GrabTicket} />
+        
+      </Router>
+
+      <footer className="footer">
+        <p>第三届青年夏日盛典暨毕业献礼 x 请回答9102</p>
+      </footer>
     </div>
   );
 }
---
+++ py-frontend/src/entry.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Entry extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div class>
+                
+            </div>
+        )
+    }
+
+}
