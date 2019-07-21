9e3e7b94dfde839632a4e57798aaf2df5c19140a
abrasumente <abrasumentee@gmail.com>
2019-07-21 12:05:13 +0800

    model update

--- pyserver/api/models.py
+++ pyserver/api/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 class Ticket(models.Model):
 
     id = models.AutoField(primary_key=True)
-    uid = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=11)
-    bought_date = models.DateTimeField(auto_now=True)
-    used = models.BooleanField(default=False)
-    used_date = models.DateTimeField(null=True)
+    phone_number = models.BigIntegerField(max_length=20)
+    password = models.BigIntegerField(max_length=20)
+    number = models.IntegerField(max_length=11)
+    t-1 = models.TextField()
+    t-2 = models.TextField()
+    checktime = models.TextField()
+    ypzt = models.IntegerField(max_length=11)
 
     def __str__(self):
         return self.uid
