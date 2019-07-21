1d503890907356962024e20132761283ff4b5626
abrasumente <abrasumentee@gmail.com>
2019-07-21 12:12:00 +0800

    remove length limit for int

--- pyserver/api/models.py
+++ pyserver/api/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Ticket(models.Model):
 
     id = models.AutoField(primary_key=True)
-    phone_number = models.BigIntegerField(max_length=20)
-    password = models.BigIntegerField(max_length=20)
-    number = models.IntegerField(max_length=11)
+    phone_number = models.BigIntegerField()
+    password = models.BigIntegerField()
+    number = models.IntegerField()
     t1 = models.TextField()
     t2 = models.TextField()
     checktime = models.TextField()
-    ypzt = models.IntegerField(max_length=11)
+    ypzt = models.IntegerField()
 
     def __str__(self):
         return self.password
